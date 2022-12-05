cb5c425d2b5ad1f814f271e0630c6780e22c371f
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-05 11:22:04 +0100

    server works

---
+++ content.js
@@ -0,0 +1,23 @@
+const message = 'Welcome to this Node site.';
+
+export const mainContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Info Site</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            padding: 0 1rem;
+        }
+    </style>
+</head>
+<body>
+    <h1>Info Site</h1>
+    <p>${message}</p>
+</body>
+</html>
+`;
